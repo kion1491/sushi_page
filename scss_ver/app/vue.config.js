9520375d092e75ba71d5212f275373967e67fdb1
kion1491 <43998132+kion1491@users.noreply.github.com>
2021-06-27 23:40:18 +0900

    vue config js move

---
+++ scss_ver/app/vue.config.js
@@ -0,0 +1,15 @@
+// vue.config.js
+module.exports = {
+    // css: {
+    //     loaderOptions: {
+    //         sass: {
+    //             data: `
+    //                 @import "@/styles/_variables.scss";
+    //                 @import "@/styles/_mixins.scss";
+    //             `
+    //         }
+    //     }
+    // }
+  outputDir: './docs',
+  publicPath: '/sushi_page/scss_ver/'  
+}
